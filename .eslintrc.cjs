1f409ba4076dfcb2ea25815cddf696f7c648fb34
Pawel Konik <konius85@gmail.com>
2022-10-14 20:54:05 +0200

    github actions

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,27 +2,27 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+  },
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier',
   ],
   rules: {
-    quotes: [ "error", "single" ]
+    quotes: ['error', 'single'],
   },
   overrides: [
     {
-      files: [
-        'cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:cypress/recommended'
-      ]
-    }
+      files: ['cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}'],
+      extends: ['plugin:cypress/recommended'],
+    },
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    ecmaVersion: 'latest',
+  },
 }
